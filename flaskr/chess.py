032464011b0953816076c7cc1107f710e4a84af9
alicjakarczewska <alicja.karczewska1@gmail.com>
2022-10-03 10:21:52 +0200

    Add functions to translate figure positions to int list, rename property in Figure class

--- flaskr/chess.py
+++ flaskr/chess.py
@@ -1,10 +1,20 @@
 from abc import ABCMeta
 
+def translate_position_to_list(pos):
+    """A function that translating position (string) to list of two ints"""
+    pos_list = [ord((pos[0].upper()))-64, int(pos[1])]
+    return pos_list
+
+def translate_list_to_position(pos_list):
+    """A function that translating list of two ints to position (string)"""
+    pos = chr(pos_list[0]+64) + str(pos_list[1])
+    return pos
+
 class Figure(metaclass=ABCMeta):
 
-    def __init__(self, position=None):
-      self._position=position
-      
+    def __init__(self, current_field=None):
+      self._current_field = current_field
+
 
     def list_available_moves():      
       logger.info("Not implemented")
@@ -12,3 +22,5 @@ class Figure(metaclass=ABCMeta):
 
     def validate_move(dest_field):
       logger.info("Not implemented")
+
+    
